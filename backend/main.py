e26364eaa929ca3022162db45f94107475d4e2ca
Vishal Sharma <vishalsharma95980@gmail.com>
2026-09-25 15:44:42 +0530

    Allow Netlify frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -26,9 +26,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://krishisetu-farm.netlify.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
